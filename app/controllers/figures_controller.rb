50e30acf03e4752f91f5a63eb3d59b56d304b7b9
github username <github email address>
2017-11-09 19:19:55 -0500

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,61 @@
+class FiguresController < ApplicationController
+
+#CREATE
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+#READ
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+#UPDATE
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+      end
+
+      if !params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+end
